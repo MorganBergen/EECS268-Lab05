24a9db186f662fbdf5ed8a2f2add8f1e453688bb
Morgan Bergen <morganmahabergen@icloud.com>
2021-11-17 10:26:07 -0600

    Initial Commit

---
+++ EECS268-Lab05/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  EECS268-Lab05
+//
+//  Created by Morgan Bergen on 11/17/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+	
+	std::cout << "Hello, World!\n";
+	return 0;
+}
+
+
